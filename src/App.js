240703f10119f45b7b64335872b86cd362d7e971
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-12-29 11:15:40 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,101 +1,18 @@
 import React, { useState } from "react";
-import "./styles.css";
-import Modal from "./Modal";
+
+const components = { v1: "", v2: "" };
 
 export default function App() {
-  const answer = 3;
-  const choices = [1, 2, 3, 4];
-
-  const [selected, setSelected] = useState([]);
-  const [result, setResult] = useState();
-
-  function select(number) {
-    setSelected([...selected, number]);
-  }
-
-  function deselect(number) {
-    const index = selected.indexOf(number);
-    if (index === -1) return;
-    const newSelected = [...selected];
-    newSelected.splice(index, 1);
-    setSelected(newSelected);
-  }
-
-  function done() {
-    const selectedTotal = selected.reduce((a, b) => a + b, 0);
-    selectedTotal === answer
-      ? setResult("correct!")
-      : setResult("incorrect...");
-  }
-
-  function reset() {
-    setSelected([]);
-    setResult();
-  }
+  const versions = Object.keys(components);
+  const [version, setVersion] = useState(versions[0]);
 
   return (
-    <div className="container">
-      {result && (
-        <Modal>
-          <div style={{ textAlign: "center" }}>
-            <h1>{result}</h1>
-            <button className="action" onClick={reset}>
-              Try again
-            </button>
-          </div>
-        </Modal>
-      )}
-
-      <div className="full-width">
-        Select numbers that add up to:
-        <br />
-        <span className="answer">{answer}</span>
-      </div>
-
-      <div className="side-by-side">
-        <div className="numbers">
-          {choices.map((number) => (
-            <button
-              key={number}
-              className="number"
-              onClick={() => select(number)}
-            >
-              {number}
-            </button>
-          ))}
-        </div>
-
-        <div className="numbers">
-          {selected.map((number, index) => (
-            <button
-              key={index}
-              className="number"
-              onClick={() => deselect(number)}
-            >
-              {number}
-            </button>
-          ))}
-        </div>
-      </div>
-
-      <div className="full-width">
-        <button className="action" onClick={reset}>
-          Reset
+    <>
+      {versions.map((version) => (
+        <button onClick={() => setVersion(version)} version={version}>
+          {version}
         </button>
-        <button className="action" onClick={done}>
-          Done
-        </button>
-      </div>
-
-      <div className="full-width footer">
-        <b>react-number-game-v1</b>
-        <br />
-        read{" "}
-        <a href="https://bouwe.io/learn-react-basics-by-creating-a-number-game">
-          this blog post
-        </a>{" "}
-        on how to build this
-      </div>
-    </div>
+      ))}
+    </>
   );
 }
---
+++ src/v2/App.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+import Modal from "./Modal";
+import { getQuestion } from "./functions";
+
+// Wat heb/ga ik aangepast aan de code:
+// 1) function getQuestion gemaakt, deze geeft object terug
+// 2) answer + choices in question object veranderd
+// 3) answer + choices worden nu destructured vanuit het question object
+// 4) useState voor question/setQuestion
+// 5) De initial state is een object met een choices property die een lege array heeft zodat de .map in de JSX het tenminste doet.
+// 6) useEffect toegevoegd met setQuestion indien result leeg.
+// 7) De reset zorgt ervoor dat result leeg is en de useEffect wat doet.
+
+export default function App() {
+  const [question, setQuestion] = useState({ choices: [] });
+  const { answer, choices } = question;
+  const [selected, setSelected] = useState([]);
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    if (!result) setQuestion(getQuestion());
+  }, [result]);
+
+  function select(number) {
+    setSelected([...selected, number]);
+  }
+
+  function deselect(number) {
+    const index = selected.indexOf(number);
+    if (index === -1) return;
+    const newSelected = [...selected];
+    newSelected.splice(index, 1);
+    setSelected(newSelected);
+  }
+
+  function done() {
+    const selectedTotal = selected.reduce((a, b) => a + b, 0);
+    selectedTotal === answer
+      ? setResult("correct!")
+      : setResult("incorrect...");
+  }
+
+  function reset() {
+    setSelected([]);
+    setResult();
+  }
+
+  return (
+    <div className="container">
+      {result && (
+        <Modal>
+          <div style={{ textAlign: "center" }}>
+            <h1>{result}</h1>
+            <button className="action" onClick={reset}>
+              Try again
+            </button>
+          </div>
+        </Modal>
+      )}
+
+      <div className="full-width">
+        Select numbers that add up to:
+        <br />
+        <span className="answer">{answer}</span>
+      </div>
+
+      <div className="side-by-side">
+        <div className="numbers">
+          {choices.map((number) => (
+            <button
+              key={number}
+              className="number"
+              onClick={() => select(number)}
+            >
+              {number}
+            </button>
+          ))}
+        </div>
+
+        <div className="numbers">
+          {selected.map((number, index) => (
+            <button
+              key={index}
+              className="number"
+              onClick={() => deselect(number)}
+            >
+              {number}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      <div className="full-width">
+        <button className="action" onClick={reset}>
+          Reset
+        </button>
+        <button className="action" onClick={done}>
+          Done
+        </button>
+      </div>
+
+      <div className="full-width footer">
+        <b>react-number-game-v2</b>
+        <br />
+        read{" "}
+        <a href="https://bouwe.io/learn-react-basics-by-creating-a-number-game">
+          this blog post
+        </a>{" "}
+        on how to build this
+      </div>
+    </div>
+  );
+}
