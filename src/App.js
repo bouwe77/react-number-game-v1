edc120aab4b4a9905ca68e2d58acbf5ce781c37f
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-12-29 16:25:53 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
+import V1 from "./v1/App";
+import V2 from "./v2/App";
 
-const components = { v1: "", v2: "" };
+const components = { v1: V1, v2: V2 };
 
 export default function App() {
   const versions = Object.keys(components);
-  const [version, setVersion] = useState(versions[0]);
+  const defaultVersion = versions[0];
+  const [version, setVersion] = useState(defaultVersion);
+
+  const Component = components[version || defaultVersion];
 
   return (
     <>
-      {versions.map((version) => (
-        <button onClick={() => setVersion(version)} version={version}>
-          {version}
-        </button>
-      ))}
+      <div>
+        {versions.map((version) => (
+          <button onClick={() => setVersion(version)} version={version}>
+            {version}
+          </button>
+        ))}
+      </div>
+      <Component />
     </>
   );
 }
